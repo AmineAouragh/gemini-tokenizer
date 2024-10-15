68637e981d1f27d133dfeed300d7d830ecfccba7
Amine Aouragh <amine.aouragh.20@gmail.com>
2024-10-15 21:46:26 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,71 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [ tokens, setTokens ] = useState(0)
+  const [ characters, setCharacters ] = useState(0)
+  const [ input, setInput ] = useState("")
+
+  useEffect(() => {
+    if (input.length > 0){
+      setCharacters(input.length) // count characters
+      setTokens(Math.floor(input.length/4)) // count tokens, each token is 4 characters
+    } else {
+      setCharacters(0)
+      setTokens(0)
+    }
+  }, [input, characters, tokens])
+
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center absolute py-4">
+      <h2 className="text-md text-gray-700 text-4xl font-bold mb-4">Gemini API Tokenizer</h2>
+      <div className="flex flex-row items-center mb-4">
+        <button type="button" className="bg-gray-50 hover:bg-gray-100 font-bold text-gray-600 hover:text-gray-700 px-3 py-2 text-sm rounded-lg mr-2">Gemini 1.5 Flash</button>
+        <button type="button" className="bg-gray-50 hover:bg-gray-100 font-bold text-gray-600 hover:text-gray-700 px-3 py-2 text-sm rounded-lg mr-2">Gemini 1.5 Flash8B</button>
+        <button type="button" className="bg-gray-50 hover:bg-gray-100 font-bold text-gray-600 hover:text-gray-700 px-3 py-2 text-sm rounded-lg mr-2">Gemini 1.5 Pro</button>
+        <button type="button" className="bg-gray-50 hover:bg-gray-100 font-bold text-gray-600 hover:text-gray-700 px-3 py-2 text-sm rounded-lg">Text Embedding & Embedding</button>
+      </div>
+      <textarea placeholder="Write or paste your text here to count tokens and characters" value={input} onChange={e => setInput(e.target.value)} className="w-1/3 outline-none italic h-80 text-gray-700 font-semibold border-2 text-lg border-gray-500 rounded-xl p-2 mb-6">
+
+      </textarea>
+      <div className="flex flex-row items-center justify-start">
+        <p className="mr-8 text-xl bg-gray-200 rounded-md px-3 py-2">Tokens: {tokens}</p>
+        <p className="text-xl bg-gray-200 rounded-md px-3 py-2">Characters: {characters}</p>
+      </div>
+      <div id="faq" className="mt-8">
+        <div id="q1" className="mb-4">
+          <p id="question1" className="text-xl font-bold mb-2">How are tokens counted?</p>
+          <p id="answer1">A token is equivalent to about 4 characters of English text for Gemini models.</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div id="q2" className="">
+          <p id="question2" className="text-xl font-bold mb-2">Is there a token limit?</p>
+          <div id="answer2">
+          <p>Of course, and here is how it might be different from a Gemini model to another:</p>
+          <ul className="mt-2" id="models">
+            <li className="font-semibold text-gray-600">Gemini 1.5 Flash:</li>
+            <div className="ml-4 flex flex-row items-center mb-2">
+              <p className="mr-6">Input token limit: <span>1,048,576</span></p>
+              <p>Output token limit: <span>8,192</span></p>
+            </div>
+            <li className="font-semibold text-gray-600">Gemini 1.5 Flash-8B:</li>
+            <div className="ml-4 flex flex-row items-center mb-2">
+              <p className="mr-6">Input token limit: <span>1,048,576</span></p>
+              <p>Output token limit: <span>8,192</span></p>
+            </div>
+            <li className="font-semibold text-gray-600">Gemini 1.5 Pro:</li>
+            <div className="ml-4 flex flex-row items-center mb-2">
+              <p className="mr-6">Input token limit: <span>2,097,152</span></p>
+              <p>Output token limit: <span>8,192</span></p>
+            </div>
+            <li className="font-semibold text-gray-600">Text Embedding and Embedding:</li>
+            <div className="ml-4 flex flex-row items-center">
+              <p className="mr-6">Input token limit: <span>2,048</span></p>
+              <p>Output dimension size: <span>768</span></p>
+            </div>
+          </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
